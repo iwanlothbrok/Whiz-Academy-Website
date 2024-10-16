2dea440cc0d5be0464895f50955c8829c8e4d85b
Iwan <iwaniwanow0345@gmail.com>
2024-10-16 14:13:38 +0300

    cleaning and downloading packages

--- whiz-academy/src/index.js
+++ whiz-academy/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
+
+import { BrowserRouter } from 'react-router-dom';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Navigation from './components/layout/Navigation';
+import Footer from './components/layout/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <div className='bg-black'>
+      <Navigation />
+      <App />
+      <Footer />
+    </div>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
