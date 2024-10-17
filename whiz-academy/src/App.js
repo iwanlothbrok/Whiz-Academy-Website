e25246a05716213b1e3b9fb2700fcb79e5790b33
Iwan <iwaniwanow0345@gmail.com>
2024-10-17 17:41:51 +0300

    app js was updated

--- whiz-academy/src/App.js
+++ whiz-academy/src/App.js
@@ -1,10 +1,19 @@
-import About from './components/layout/About';
+import CTAButton from './components/CTAButton';
+import About from './components/about/About';
+import Features from './components/layout/Features';
+import Home from './components/layout/Home';
+import Testimonials from './components/layout/Testemonials';
 import './index.css';
 
 function App() {
   return (
-    <div className="bg-black">
-      <About/>
+    <div className="">
+      <Home />
+      {/* <About /> */}
+      <Features />
+      <div className='bg-gradient-to-b from-green-950 to-black p-3'>
+        <Testimonials />
+      </div>
     </div>
   );
 }
