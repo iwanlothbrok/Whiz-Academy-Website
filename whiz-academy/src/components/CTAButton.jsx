7b4fdbd5b9cf1e0fc3835a76e9fa32a97931af23
Iwan <iwaniwanow0345@gmail.com>
2024-10-18 17:07:36 +0300

    Update CTAButton.jsx

--- whiz-academy/src/components/CTAButton.jsx
+++ whiz-academy/src/components/CTAButton.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 const CTAButton = () => {
     return (
         <motion.div
-            className="fixed top-1/2 right-0 transform -translate-y-1/2 group"
+            className="fixed top-1/2 right-0 transform -translate-y-1/2 group z-50"
             initial={{ x: '50%' }}  // Initially more hidden on mobile
             animate={{ x: '55%' }}  // Stay further hidden for smaller screens
             whileHover={{ x: '0%' }}  // Fully visible on hover
