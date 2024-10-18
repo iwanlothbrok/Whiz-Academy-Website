a08b0f97f86452bf6cd7cb59af2c814056f58a48
Iwan <iwaniwanow0345@gmail.com>
2024-10-18 17:07:57 +0300

    Update App.js

--- whiz-academy/src/App.js
+++ whiz-academy/src/App.js
@@ -1,7 +1,9 @@
 import CTAButton from './components/CTAButton';
 import About from './components/about/About';
 import Features from './components/layout/Features';
+import FrequentlyAskedQuestions from './components/layout/FrequentlyAskedQuestions';
 import Home from './components/layout/Home';
+import RegisterCTA from './components/layout/RegisterCTA';
 import Testimonials from './components/layout/Testemonials';
 import './index.css';
 
@@ -9,11 +11,14 @@ function App() {
   return (
     <div className="">
       <Home />
+
       {/* <About /> */}
       <Features />
-      <div className='bg-gradient-to-b from-green-950 to-black p-3'>
+      <div className='bg-gradient-to-b from-green-950 to-black p-3' >
         <Testimonials />
       </div>
+      <FrequentlyAskedQuestions />
+      <RegisterCTA />
     </div>
   );
 }
